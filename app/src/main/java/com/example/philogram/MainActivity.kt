3336138ffea6828a7e7c673d72554ae761df9a70
DESKTOP-D2CEB08\danny <dannyjoo2003@gmail.com>
2023-08-14 19:48:17 +0900

    FEAT: 디테일 페이지 / 마이페이지 추가

--- app/src/main/java/com/example/philogram/MainActivity.kt
+++ app/src/main/java/com/example/philogram/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.philogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.detail_activity)
+
+        val edit_txt = findViewById<TextView>(R.id.edit_txt)
+
+        edit_txt.setOnClickListener {
+            showFeedDialog()
+        }
+    }
+
+    private fun showFeedDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.feed_dialog, null)
+
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+
+        val alertDialog = dialogBuilder.create()
+        alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val dialog_btn = dialogView.findViewById<Button>(R.id.dialog_btn) // dialogView에서 버튼을 찾음
+        dialog_btn.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
     }
 }
