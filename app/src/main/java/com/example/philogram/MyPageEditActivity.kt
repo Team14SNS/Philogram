a57e81b5d70e9c97a86e29f5d2c594fed505a3a6
mwkimm <94061061+mwkimm@users.noreply.github.com>
2023-08-14 20:24:18 +0900

    FEAT: 편집페이지 기능구현

--- app/src/main/java/com/example/philogram/MyPageEditActivity.kt
+++ app/src/main/java/com/example/philogram/MyPageEditActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MyPageEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_my_page_edit)
 
 
 
